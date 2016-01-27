5efeea6943f099aefb2861d3c6eb8ec97b7eca1a
niki_main <niki_berman@bitbucket.com>
2016-01-27 15:38:35 +0200

    added get_leaf_order

--- draw_tree.py
+++ draw_tree.py
@@ -5,12 +5,6 @@ import json
 import argparse
 import ete2
 import math
-from Bio import Phylo
-
-
-def load_tree_with_data(tree_file):
-    tree = Phylo.read(tree_file, "newick")
-    return tree
 
 
 def load_json(file):
@@ -25,6 +19,21 @@ def reorder_tree(order_vector_file):
 
     reorder_t = reorder_data
     return reorder_t
+
+
+def strip_keys_to_ids(table):
+    samp_id = table.keys()[0]
+    if samp_id.isdigit():
+        return table
+    clean_table = dict()
+    for key, value in table.iteritems():
+        list_key = list(key)
+        for i, digit in enumerate(list_key):
+            if digit.isdigit():
+                new_key = ''.join(list_key[i:])
+                break
+        clean_table[new_key] = value
+    return clean_table
 
 
 def to_hex(n):
@@ -43,23 +52,19 @@ def get_cells_colors(cell_colors_file):
     cells_colors = colors['cellColors']
     new_dict_colors = dict()
     for n in cells_colors.keys():
-        new_cell_name = n.split('c')
-        new_dict_colors[new_cell_name[1]] = hex_to_rgb(cells_colors[n])
+        new_dict_colors[n] = hex_to_rgb(cells_colors[n])
 
     return new_dict_colors
 
 
-def get_cluster_colors(clustering_colors_file):
-    colors = load_json(clustering_colors_file)
-    cluster_colors = colors['BranchColor']
-    new_dict_colors = dict()
-    for group in cluster_colors.keys():
-        if not cluster_colors[group]:
-            continue
-        for branch in cluster_colors[group].keys():
-            new_dict_colors[branch] = hex_to_rgb(cluster_colors[branch])
+def get_cells_labels(leaf_labels_file):
+    labels = load_json(leaf_labels_file)
+    cells_labels = strip_keys_to_ids(labels['leafLabels'])
+    new_dict_labels = dict()
+    for n in cells_labels.keys():
+        new_dict_labels[n] = cells_labels[n]
 
-    return new_dict_colors
+    return new_dict_labels
 
 
 def get_branch_width(clustering_sizes_file):
@@ -75,15 +80,40 @@ def get_branch_width(clustering_sizes_file):
     return new_dict_width
 
 
+def get_leaf_order(order_vector_file):
+    order_vector = load_json(order_vector_file)
+    re_order = strip_keys_to_ids(order_vector['reOrder'])
+    # re_order = order_vector['reOrder']
+    order_dict = dict()
+    for n in re_order.keys():
+        order_dict[n] = re_order[n]
+
+    return order_dict
+
+
+def get_cluster_colors(clustering_colors_file):
+    colors = load_json(clustering_colors_file)
+    cluster_colors = colors['BranchColor']
+    new_dict_colors = dict()
+    for group in cluster_colors.keys():
+        if not cluster_colors[group]:
+            continue
+        for branch in cluster_colors[group].keys():
+            new_dict_colors[branch] = hex_to_rgb(cluster_colors[group][branch])
+
+    return new_dict_colors
+
+
 def tree_draw(tree_file,
               tree_name,
-              order_vector_file,
-              cell_colors_file,
-              clustering_colors_file,
-              clustering_sizes_file,
-              intermediate_node_sizes_file,
-              intermediate_node_labels_file,
-              leaf_labels_file):
+              order_vector_file=None,
+              cell_colors_file=None,
+              clustering_colors_file=None,
+              clustering_sizes_file=None,
+              intermediate_node_sizes_file=None,
+              intermediate_node_labels_file=None,
+              leaf_labels_file=None
+              ):
 
     t = ete2.Tree(newick=tree_file, format=1)
     ts = ete2.TreeStyle()
@@ -93,9 +123,22 @@ def tree_draw(tree_file,
     for n in t.traverse():
         styles[n.name] = ete2.NodeStyle()
 
+    # reorder the tree by pre-proses if possible
     if order_vector_file:
-        order = ete2.NodeStyle()
-        order = reorder_tree(order_vector_file)
+        leaf_order = get_leaf_order(order_vector_file)
+        for n in t.traverse('postorder'):
+            if n.get_descendants():
+                a = ''
+                for leaf in n.get_descendants(strategy='levelorder'):
+                    if leaf.is_leaf():
+                        if not a:
+                            a = leaf
+                b = n.get_descendants(strategy='levelorder')[-1]
+
+                if a.is_leaf() and b.is_leaf():
+                    if leaf_order[a.name] > leaf_order[b.name]:
+                        left, right = n.children
+                        n.children = [right,left]
 
     # add colors to the leafs
     if cell_colors_file:
@@ -138,47 +181,20 @@ def tree_draw(tree_file,
 
     # add new leaf labels
     if leaf_labels_file:
-        pass
+        cells_labels = get_cells_labels(leaf_labels_file)
+        ts.show_leaf_name = False
+        for name, label in cells_labels.iteritems():
+            nodes = t.search_nodes(name=name)
+            assert len(nodes) == 1
+            node = nodes[0]
+            if cells_colors:
+                name_face = ete2.faces.TextFace(cells_labels[name], fsize=8, fgcolor=cells_colors[name])
+            else:
+                name_face = ete2.faces.TextFace(cells_labels[name], fsize=8)
+
+            node.add_face(name_face, column=0)
 
     t.render("%%inline", tree_style=ts)
-
-
-def analyse_tree(tree_file,
-                 tree_name,
-                 order_vector_file=None,
-                 cell_colors_file=None,
-                 clustering_colors_file=None,
-                 clustering_sizes_file=None,
-                 intermediate_node_sizes_file=None,
-                 intermediate_node_labels_file=None,
-                 leaf_labels_file=None
-                 ):
-    """
-    preparing all the data for drawing a tree in draw_tree (for now, it's matlab)
-    :param tree_file: file name of newick tree
-    :param tree_name: name for the tree fig that is generated
-    :param order_vector_file:
-    :param cell_colors_file:
-    :param clustering_colors_file:
-    :param intermediate_node_sizes_file:
-    :param intermediate_node_labels_file:
-    :param leaf_labels_file:
-    :param bootstraping_file:
-    :return:
-    """
-    t = load_tree_with_data(tree_file)  # any tree in newick form
-    cell_colors = cell_colors_file
-    tree_name = ''.format()
-    # run matlab groups
-    tree_draw(tree_file,
-              tree_name,
-              order_vector_file,
-              cell_colors_file,
-              clustering_colors_file,
-              clustering_sizes_file,
-              intermediate_node_sizes_file,
-              intermediate_node_labels_file,
-              leaf_labels_file)
 
 
 if '__main__' == __name__:
@@ -206,7 +222,7 @@ if '__main__' == __name__:
     intermediate_node_labels_file = args.intermediate_node_labels_file
     leaf_labels_file = args.leaf_labels_file
 
-    analyse_tree(tree_file, tree_name,
+    tree_draw(tree_file, tree_name,
                  order_vector_file=order_vector_file,
                  cell_colors_file=cell_colors_file,
                  clustering_colors_file=clustering_colors_file,
